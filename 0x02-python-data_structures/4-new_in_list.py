2d23e76f43db970dffa49f942398bab9bf4c5a1e
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-01 03:54:52 +0000

    func that replaces an element in a list at specified dest

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    next_list = my_list.copy()
+    if idx < 0:
+        return next_list
+    elif idx >= len(my_list):
+        return next_list
+    else:
+        next_list[idx] = element
+        return next_list
