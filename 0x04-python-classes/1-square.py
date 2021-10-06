dc863df15f8343ae3d25ee99e04a37903cb9a447
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-06 03:32:57 +0000

    1 self.__size fix and delete blank line.1

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -5,8 +5,7 @@
 class Square:
     """ Size for square parameters """
     def __init__(self, size=0):
-        self.size__size = size
-
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
