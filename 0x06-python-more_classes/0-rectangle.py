f76290d985ce83f445ac4c495b6d48c9148a982c
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-13 04:43:11 +0000

    wasn't outputting height ValueError, realized line 22 was width instead of height, fixed

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
 
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        if width < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
