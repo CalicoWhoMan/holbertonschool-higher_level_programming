11936f6a8ad6092433bc179d7cb2b86cb4626db1
calicowhoman <mikeyshrier@gmail.com>
2021-10-13 18:30:21 -0700

    03 F that is replicated of 3 but adds instance of instance of class inherited from specific class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+F that's True if obj is an instance of or is an instance of a class from
+the specified class otherwise False
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Documentation for this function
+    """
+    return(isinstance(obj, a_class))
