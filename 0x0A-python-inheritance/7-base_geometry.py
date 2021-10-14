37578ae0335110769e9f3d775897404f1248b556
calicowhoman <mikeyshrier@gmail.com>
2021-10-13 19:26:06 -0700

    07 replicated 06 and added F integer_validator with Name and Value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Replicate 06 and adds F integer_validator with TypeError and ValueError
+"""
+
+
+class BaseGeometry:
+    """
+    Empty empty class class
+    """
+    def area(self):
+        """
+        Documentation my Documentation
+        """
+        raise Exception("area() is not implemented")
+
+        def integer_validator(self, name, value):
+            """
+            Validates Value, Name is a string
+            """
+            if type(value) is not int:
+                raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
