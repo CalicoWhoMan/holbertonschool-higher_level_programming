4148a5ec1d717e2a3d6548773c91fe80c05d9fb7
calicowhoman <mikeyshrier@gmail.com>
2021-10-13 18:05:25 -0700

    01 F def print_sorted(self):

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Class called MyList that inherits from List
+F -- def print_sorted(self)
+"""
+
+
+def print_sorted(self):
+    """
+    Printed List in ascending sorted order
+    """
+    print(sorted(self))
