4673648205ad0d712c5704ac9ab61fa4d2f07a7a
calicowhoman <mikeyshrier@gmail.com>
2021-10-13 19:32:19 -0700

    fixed indentation issues

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,17 +8,18 @@ class BaseGeometry:
     """
     Empty empty class class
     """
+
     def area(self):
         """
         Documentation my Documentation
         """
         raise Exception("area() is not implemented")
-
-        def integer_validator(self, name, value):
-            """
-            Validates Value, Name is a string
-            """
-            if type(value) is not int:
-                raise TypeError("{} must be an integer".format(name))
-            if value <= 0:
-                raise ValueError("{} must be greater than 0".format(name))
+    
+    def integer_validator(self, name, value):
+        """
+        Validates Value, Name is a string
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
