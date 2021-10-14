dff28c3130f007d700ea686cc5c1fbb498b9657b
calicowhoman <mikeyshrier@gmail.com>
2021-10-13 19:05:43 -0700

    06 Replicated 05 with Raised Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Replicate 05 and add Area that raises an Exception with a message
+"""
+
+
+class BaseGeometry:
+    """
+    Empty empty class class
+    """
+    def area(self):
+        """
+        Documentation my Documentation
+        """
+        raise Exception("area() is not implemented")
