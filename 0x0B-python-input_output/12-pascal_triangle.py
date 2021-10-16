2dd4a7dbb4f078218966eadd1cd4a396c972c3e8
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-16 04:41:48 +0000

    open 12 file

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns list of ints representing triangle
+    """
+
+
+
