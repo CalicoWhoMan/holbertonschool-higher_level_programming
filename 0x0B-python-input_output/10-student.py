848227944a31187f7367728e21bdb78f515e4a30
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-16 04:32:47 +0000

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by: (based on 9-student.py)
+"""
+
+
+class Student:
+    """
+    class student student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        class specifications for student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves dictonary reps of student class
+        """
+
+
+        strs = {}
+        
+
+        if attrs is not None:
+            return self.__dict__
+            for varis in attrs:
+                if varis in self.__dict__:
+                    strs[key] = self.dict__dict__[key]
+            return strs
+        else:
+            return vars(self)
