71b449a3e85b7adbbe7213fd8466b355d9f48fe0
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-16 02:03:18 +0000

    00, fixed main file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     """
     Read file and print to stdout
     """
-    with open(filename, encoding="utf-8" as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
