94d4903730e38be8369e6e23a1edaf3197889685
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-22 04:33:32 +0000

    added json string and list dicts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Class Base
 """
 
 
+import json
+
+
 class Base:
     """
     Class for file Base
@@ -20,3 +23,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string rep of list_dictionaries
+        """
+        listdict2 = []
+        if list_dictionaries is None or list_dictionaries == "":
+            return listdict2
+        else:
+            listdict2 = json.dumps(list_dictionaries)
+        return listdict2
