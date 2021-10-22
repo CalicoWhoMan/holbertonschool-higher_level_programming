c61cf5fe5839fb22d05f47cfffca8e21e78f48fa
CalicoWhoMan <mikeyshrier@gmail.com>
2021-10-22 03:30:15 +0000

    square docu.2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-"""
-setting up class for square
-"""
+    """
+    setting up class for square
+    """
 
 
     def __init__(self, size, x=0, y=0, id=None):
