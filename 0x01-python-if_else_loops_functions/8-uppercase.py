1697d9d41a429b836bd70d598b571ab9c2c5b84f
CalicoWhoMan <mikeyshrier@gmail.com>
2022-01-04 20:27:45 -0800

    fixed -32 and first line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if 97 <= ord(char) <= 122:
-            a = chr(ord(x) -32)
-        else:
-            a = x
-        print("{}".format(a), end="")
-    print("")
+        if ord(x) in range (97, 123):
+            x = chr(ord(x)- 32)
+        print("{}".format(x), end="")
+    print()
