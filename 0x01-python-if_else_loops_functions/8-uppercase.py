c38dbe877d47db810e30d91ef3301b3cd13766a1
CalicoWhoMan <mikeyshrier@gmail.com>
2022-01-04 20:29:40 -0800

    1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if ord(x) in range (97, 123):
+        if ord(x) in range (96, 123):
             x = chr(ord(x)- 32)
         print("{}".format(x), end="")
     print()
