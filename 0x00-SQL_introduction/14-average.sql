1ae89ee1a9e2682f99b1b7f6af098bd5d3bf5a22
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-09 21:45:53 -0800

    added last tasks

---
+++ 0x00-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Script that finds the average  score of all records in table second_table
+SELECT AVG(score)
+AS average
+FROM second_table;
---
+++ 0x00-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- Script that lists the # of records with the same score in second_table
+SELECT score,
+COUNT(score)
+AS number
+FROM second_table
+GROUP BY score
+ORDER BY number
+DESC;
