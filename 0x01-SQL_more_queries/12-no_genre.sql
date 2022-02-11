7dda25e6bd0428dff56b8ebb96de2b9d86d3d330
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-10 21:37:15 -0800

    Imports the DB dump from hbtn_0d_tvshows to your server

---
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Import the DB dump from 'hbtn_0d_tvshows' to your server
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
