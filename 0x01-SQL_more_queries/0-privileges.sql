affcd575d2e2309f66ba33f4aaea940155482c31
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-11 16:50:26 -0800

    GH issues, added second comment line

--- 0x01-SQL_more_queries/0-privileges.sql
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Script that lists all privileges of 'user_0d_1' & 'user_0d_2'
+-- Script that lists all priveleges of the two users
+-- user 1 and user 2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
