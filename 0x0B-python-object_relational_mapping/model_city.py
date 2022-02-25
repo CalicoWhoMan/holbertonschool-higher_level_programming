c5d9c6f84a3d96b393f0b72a401568b5644c057f
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-24 21:04:49 -0800

    10-14 & city_class added imports + points ->

--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
 """PY file that contains the class def of a 'City'"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):i
+    """class for City"""
+    __tablename__ = 'cities'
