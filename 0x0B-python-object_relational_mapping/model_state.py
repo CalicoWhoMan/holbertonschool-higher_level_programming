d76cced5912cd6b6a2d408753c9ccbcb48cd72cc
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-24 17:32:36 -0800

    task 6

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-"""A PY file that contains the class def of a 'State' and an instance"""
+"""A PY file that contains the class def of 'State'"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class that inherits from 'Base', links to SQL table 'states"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key = True, nullable = False)
+    name = Column(String(128), nullable = False)
