b6b67ab4d0def739d7c0b6c80479a481faff3e01
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-24 17:31:03 -0800

    task 2

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """Script that takes in arg and displays values in 'states' where 'name=arg'"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                    passwd=sys.argv[2], database=sys.argv[3])
+    mem = db.cursor()
+    mem.execute("""SELECT * FROM states WHERE name = '{}'
+    ORDER BY states.id ASC""".format(sys.argv[4]))
+
+    for row in mem.fetchall():
+        print(row)
+
+    mem.close()
+    db.close()
