f9030cfd22b61ae46b4d635933e01c72fe69bc3d
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-24 17:31:25 -0800

    task 4

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 """Script that lists all 'cities' from the database"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                    passwd=sys.argv[2], database=sys.argv[3])
+    mem = db.cursor()
+    mem.execute("""SELECT cities.id, cities.name, states.name
+    FROM cities JOIN states ON cities.state_id = states.id
+    ORDER BY states.id ASC""")
+
+    for row in mem.fetchall():
+        print(row)
+
+    mem.close()
+    db.close()
