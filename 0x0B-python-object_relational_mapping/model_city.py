230a005cf151e3f7f0c2d24f235a6c1b48cbfd0d
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-25 20:19:02 -0800

    adding class City for 14

--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """PY file that contains the class def of a 'City'"""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
 Base = declarative_base()
 
-class City(Base):i
+class City(Base):
     """class for City"""
     __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
