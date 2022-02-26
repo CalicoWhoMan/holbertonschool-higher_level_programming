ae9da4def902a2c9c617a5d7332aaf77ffc22307
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-25 20:19:57 -0800

    Prints ALL City objs from the db / task 14

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,6 +5,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
 
@@ -14,3 +15,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    for state, city in session.query(State, City)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close
