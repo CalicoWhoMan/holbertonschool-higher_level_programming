6b92e6192149629d31f969718ea8a41053828879
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-25 19:36:56 -0800

    task 10

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,3 +14,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
