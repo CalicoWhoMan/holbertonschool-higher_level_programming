a3f914429266db968ff209e6735db80e735425db
CalicoWhoMan <mikeyshrier@gmail.com>
2022-02-25 20:18:37 -0800

    task 13 / del State objs containing letter 'a'

--- 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,3 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    for instance in session.query(State).filter(State.name.contains('a')):
+        session.delete(instance)
+    session.commit()
+    session.close()
