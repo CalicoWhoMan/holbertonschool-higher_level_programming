95725ba4fe643b6d7542846a0c2f56c714318481
CalicoWhoMan <mikeyshrier@gmail.com>
2022-07-07 04:37:55 +0000

    rework 10

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3
-"""Script Script"""
-
-import urllib.request
-import urllib.parse
+#!/usr/bin/python
+"""github id displays"""
+import requests
 import sys
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    nic = requests.get('https://api.github.com/user',
+                     auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(nic.json().get('id'))
