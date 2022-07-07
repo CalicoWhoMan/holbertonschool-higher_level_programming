12374dd7d87fc70a21a683cb19fac15deb392a05
CalicoWhoMan <mikeyshrier@gmail.com>
2022-07-07 04:38:57 +0000

    rework 10.1

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     nic = requests.get('https://api.github.com/user',
-                     auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+                       auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
     print(nic.json().get('id'))
