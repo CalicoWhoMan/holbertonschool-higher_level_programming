f66c26c3e70d4c904ba4cc87fc1f5866db011cc0
CalicoWhoMan <mikeyshrier@gmail.com>
2022-07-07 03:34:59 +0000

    rework 3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Script Script"""
-
-import urllib.request
-import urllib.parse
-import sys
-
+"""Take url, send req to url and display response"""
 if __name__ == "__main__":
+    from sys import argv
+    import urllib.request
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except Exception as excep:
+        print("Error code: {}".format(excep.code))
