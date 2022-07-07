0e4017bb6e7600d0b1a9a89d110e11b00fb201a3
CalicoWhoMan <mikeyshrier@gmail.com>
2022-07-07 04:06:18 +0000

    rework 7

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Script Script"""
-
-import urllib.request
-import urllib.parse
-import sys
-
+"""Error code - takes URL, sends req to URL and displays response"""
 if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    if r.status_code <= 400:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
